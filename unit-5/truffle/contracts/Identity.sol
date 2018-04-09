93a10151ba03dadb7a6a83864be52f88ead9be6f
Mircea Pasoi <mircea.pasoi@gmail.com>
2018-04-08 18:01:21 -0700

    Update Identity constructor

--- unit-5/truffle/contracts/Identity.sol
+++ unit-5/truffle/contracts/Identity.sol
@@ -8,10 +8,14 @@ import "./MultiSig.sol";
 import "./ClaimManager.sol";
 
 contract Identity is Destructible, KeyManager, KeyGetters, MultiSig, ClaimManager {
-    function Identity(bytes32[] keys, uint256[] purposes, uint256[] keyTypes) public {
-        // TODO: pass some initial keys in the constructor
-        // TODO: pass some initial claims in the constructor
-
+    function Identity(
+        bytes32[] keys,
+        uint256[] purposes,
+        uint256[] keyTypes
+    )
+        public
+    {
+        // Validate keys are sorted and unique
         require(keys.length == purposes.length);
         require(purposes.length == keyTypes.length);
         for (uint i = 1; i < keys.length; i++) {
@@ -25,10 +29,12 @@ contract Identity is Destructible, KeyManager, KeyGetters, MultiSig, ClaimManage
         // Supports both ERC 725 & 735
         supportedInterfaces[ERC725ID() ^ ERC735ID()] = true;
 
-
         if (keys.length == 0) {
-            // Add key that deployed the contract
-            _addKey(addrToKey(msg.sender), MANAGEMENT_KEY, ECDSA_TYPE);
+            bytes32 senderKey = addrToKey(msg.sender);
+            // Add key that deployed the contract for MANAGEMENT, ACTION, CLAIM
+            _addKey(senderKey, MANAGEMENT_KEY, ECDSA_TYPE);
+            _addKey(senderKey, ACTION_KEY, ECDSA_TYPE);
+            _addKey(senderKey, CLAIM_SIGNER_KEY, ECDSA_TYPE);
         } else {
             // Add constructor keys
             for (i = 0; i < keys.length; i++) {
